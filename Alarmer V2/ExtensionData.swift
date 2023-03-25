4a3786f64a9eeffdd3e1a6de67e5ed265705dc84
mofarra <105955097+mofarra@users.noreply.github.com>
2023-03-25 13:53:50 -0500

    Update and rename Extension to ExtensionData.swift

---
+++ Alarmer V2/ExtensionData.swift
@@ -0,0 +1,46 @@
+class UserNotification {
+    
+    static let shared = UserNotification()
+    
+    let current = UNUserNotificationCenter.current()
+    let content = UNMutableNotificationContent()
+    
+    func addNotificationRequest(alarm: AlarmInfo, selectDays: Set<Day>) {
+        //        current.removeAllPendingNotificationRequests()
+        content.title = "Clock"
+        content.subtitle = "Alarm"
+        content.categoryIdentifier = "alarm"
+        content.sound = UNNotificationSound(named: UNNotificationSoundName("123.wav"))
+        
+        let calendar = Calendar.current
+        let hour = calendar.component(.hour, from: alarm.date)
+        let minute = calendar.component(.minute, from: alarm.date)
+        
+        var dateComponents = DateComponents()
+        dateComponents.hour = hour
+        dateComponents.minute = minute
+        
+        if selectDays.isEmpty {
+            triggerRequest(dateComponents: dateComponents, alarm: alarm, isRepeat: false)
+        } else {
+            let weekdays = selectDays.map { $0.componentWeekday }
+            weekdays.forEach { weekDay in
+                dateComponents.weekday = weekDay
+                triggerRequest(dateComponents: dateComponents, alarm: alarm)
+            }
+        }
+    }
+    
+    func triggerRequest(dateComponents: DateComponents, alarm: AlarmInfo, isRepeat: Bool = true) {
+        let trigger = UNCalendarNotificationTrigger(dateMatching: dateComponents, repeats: isRepeat)
+        let request = UNNotificationRequest(identifier: alarm.id.uuidString, content: content, trigger: trigger)
+        current.add(request) { error in
+            if(error == nil){
+                print("successfully")
+            }else{
+                print("error")
+            }
+        }
+    }
+}
+
